23b32e69bd79765ee30eb3b40ccfba2881edb0d0
Ivan Santos <pragmaticivan@gmail.com>
2013-11-26 01:00:36 -0600

    Correção de alias para a tarefa watch, existe uma sobrecarga na classe ao usar o nome do registro da tarefa com a mesma string do nome da tarefa

--- grunt-boilerplate-sass/Gruntfile.js
+++ grunt-boilerplate-sass/Gruntfile.js
@@ -176,7 +176,7 @@ module.exports = function(grunt) {
     grunt.registerTask( "default", [ "jshint", "compass", "uglify" ] );
 
     // Observar mudanças dos arquivos
-    grunt.registerTask( "watch", [ "watch" ]);
+    grunt.registerTask( "watch-files", [ "watch" ]);
 
     // Optimizar imagens
     grunt.registerTask( "optimize", [ "imagemin", "notify:image" ] );
